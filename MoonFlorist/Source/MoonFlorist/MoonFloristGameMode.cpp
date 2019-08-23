3ba60cbe4cb8b1dd9a36ede34332f5d10a58f48d
Steven <stevenzhaooooo@gmail.com>
2019-08-24 11:31:09 +1200

    Player can move - Rebound all movement controls Fixed Loading current level with the current gamemode C++ character and gamemode are linking properly

--- MoonFlorist/Source/MoonFlorist/MoonFloristGameMode.cpp
+++ MoonFlorist/Source/MoonFlorist/MoonFloristGameMode.cpp
@@ -9,8 +9,8 @@ AMoonFloristGameMode::AMoonFloristGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = AMoonFloristCharacter::StaticClass();
 
 	// use our custom HUD class
 	HUDClass = AMoonFloristHUD::StaticClass();
