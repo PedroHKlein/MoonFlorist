f9797cdff8cb7c5733522120f62e25ae9db0f5cb
Steven <stevenzhaooooo@gmail.com>
2019-10-07 16:27:43 +1300

    WIP - Crosshair

--- MoonFlorist/Source/MoonFlorist/MoonFloristHUD.cpp
+++ MoonFlorist/Source/MoonFlorist/MoonFloristHUD.cpp
@@ -12,24 +12,55 @@ AMoonFloristHUD::AMoonFloristHUD()
 	// Set the crosshair texture
 	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
+	
+	// Set the idle crosshair texture
+	static ConstructorHelpers::FObjectFinder<UTexture2D> IdleCH_Obj(TEXT("/Game/User_Interface/Crosshair/crosshair_white"));
+	Idle_CH = IdleCH_Obj.Object;
+
+	// Set the look at crosshair texture
+	static ConstructorHelpers::FObjectFinder<UTexture2D> LookAtCH_Obj(TEXT("/Game/User_Interface/Crosshair/crosshair_blue"));
+	LookAt_CH = LookAtCH_Obj.Object;
+
+	// Set the in range crosshair texture
+	static ConstructorHelpers::FObjectFinder<UTexture2D> InRangeCH_Obj(TEXT("/Game/User_Interface/Crosshair/crosshair_blueflower"));
+	InRange_CH = InRangeCH_Obj.Object;
+
+
+	Current_CH = Idle_CH;
+	//Decreasing Alpha of the Crosshair, making it semi transparent
+	Current_CH->AdjustMaxAlpha = 0.3f;
+
+
+	
 }
 
 
 void AMoonFloristHUD::DrawHUD()
 {
 	Super::DrawHUD();
-
+	
 	// Draw very simple crosshair
-
+	const FVector2D ViewportSize = FVector2D(GetWorld()->GetGameViewport()->Viewport->GetSizeXY());
 	// find center of the Canvas
-	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
-
+	const FVector2D Center(Canvas->ClipX *0.5f, Canvas->ClipY * 0.5f);
 	// offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
-	const FVector2D CrosshairDrawPosition( (Center.X),
-										   (Center.Y + 20.0f));
+	const FVector2D CrosshairDrawPosition(Center.X - (Current_CH->GetSurfaceWidth() * 0.5f) ,
+										  Center.Y - (Current_CH->GetSurfaceHeight() * 0.5f));
 
 	// draw the crosshair
-	FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
+	FCanvasTileItem TileItem(CrosshairDrawPosition, Current_CH->Resource, FLinearColor::White);
 	TileItem.BlendMode = SE_BLEND_Translucent;
+	//TileItem.Size *= 0.5f;
+	//TileItem.PivotPoint = FVector2D(Current_CH->GetSizeX() * .5f, Current_CH->GetSizeY() * .5f);
 	Canvas->DrawItem( TileItem );
 }
+
+void AMoonFloristHUD::ToggleAlpha(bool _changeState)
+{
+}
+
+void AMoonFloristHUD::ChangeState(FHitResult _target)
+{
+}
+
+
--- MoonFlorist/Source/MoonFlorist/MoonFloristHUD.h
+++ MoonFlorist/Source/MoonFlorist/MoonFloristHUD.h
@@ -17,7 +17,21 @@ public:
 	/** Primary draw call for the HUD */
 	virtual void DrawHUD() override;
 
+	UFUNCTION(BlueprintCallable, Category = "Crosshair")
+		void ToggleAlpha(bool _changeState);
+	UFUNCTION(BlueprintCallable, Category = "Crosshair")
+		void ChangeState(FHitResult _target);
+
 private:
+	/** Current Crosshair */
+	class UTexture2D* Current_CH;
+	/** Idle Crosshair*/
+	class UTexture2D* Idle_CH;
+	/** Crosshair when player looks at a interactable Object*/
+	class UTexture2D* LookAt_CH;
+	/** Crosshair when player is in range to interact with the object*/
+	class UTexture2D* InRange_CH;
+	
 	/** Crosshair asset pointer */
 	class UTexture2D* CrosshairTex;
 
