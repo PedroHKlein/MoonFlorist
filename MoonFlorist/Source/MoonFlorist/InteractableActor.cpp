5c8d20a83da1f7c48e988ffdd58e93118f57a6ff
Steven <stevenzhaooooo@gmail.com>
2019-10-29 18:19:53 +1300

    setup

--- MoonFlorist/Source/MoonFlorist/InteractableActor.cpp
+++ MoonFlorist/Source/MoonFlorist/InteractableActor.cpp
@@ -8,6 +8,9 @@ AInteractableActor::AInteractableActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	SceneComp = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComp"));
+	SceneComp->SetupAttachment(RootComponent);
+
 
 }
 
