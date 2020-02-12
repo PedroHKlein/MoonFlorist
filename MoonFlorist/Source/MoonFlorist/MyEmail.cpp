b79e7f887dc889959f491302efde81bb6597c6dd
Mark <tremere.fist@gmail.com>
2020-02-12 16:58:46 +1300

    Email back end ground work

---
+++ MoonFlorist/Source/MoonFlorist/MyEmail.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyEmail.h"
+
+// Sets default values
+AMyEmail::AMyEmail()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyEmail::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyEmail::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
